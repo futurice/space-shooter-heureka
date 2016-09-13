425e7174dfd51f86a5a5d1189c415ec2c13c90f9
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-09-13 17:18:45 +0300

    Updated background, put some rotation there

---
+++ Assets/Scripts/BackgroundBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundBehaviour : MonoBehaviour {
+
+	public float _rotationSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.Rotate(0.0f, 0.0f, _rotationSpeed * Time.deltaTime);
+	}
+}
