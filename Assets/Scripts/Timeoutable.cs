5b9f0f48d3eb15fe91fbf262cca4261aaddfdf84
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-09-20 19:35:47 +0300

    Added missing files from previous commits

---
+++ Assets/Scripts/Timeoutable.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Timeoutable : MonoBehaviour {
+	protected float _lifetimeInSecs = 0.0f;
+	private List<TimeoutListener> _listeners = new List<TimeoutListener>();
+
+	public void addTimeoutListener(TimeoutListener l) {
+		_listeners.Add(l);
+	}
+
+	public interface TimeoutListener {
+		void timeoutElapsed(Timeoutable t);
+	}
+
+	public virtual void Update() {
+		if (shouldReset()) {
+			reset();
+		}
+		else {
+			_lifetimeInSecs += Time.deltaTime;
+		}
+
+		if (_lifetimeInSecs >= getTimeout()) {
+			foreach(TimeoutListener l in _listeners) {
+				l.timeoutElapsed(this);
+			}
+		}
+	}
+
+	public virtual float getTimeout() {
+		return 10.0f;
+	}
+
+	public virtual bool shouldReset() {
+		return false;
+	}
+
+	public virtual void reset() {
+		_lifetimeInSecs = 0.0f;
+	}
+}
