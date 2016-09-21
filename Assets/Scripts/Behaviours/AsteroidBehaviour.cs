c115bb80732f8b9afa855d831ecff94e28e98456
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-09-21 21:28:51 +0300

    First draft version of floating asteroids

---
+++ Assets/Scripts/Behaviours/AsteroidBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidBehaviour : MonoBehaviour {
+	[SerializeField]
+	private float _spinMagnitude = 5.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * _spinMagnitude;
+		//place outside of bounds and calc some constant velocity, or do it in the game manager
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "projectile") {
+			GameManager.Instance.destroyWithExplosion(this.gameObject);
+			Destroy(other.gameObject);
+		}
+	}
+}
