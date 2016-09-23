dd46e13e7fd1fbd9eb83ff4c179992e72193d2dd
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-09-23 19:38:55 +0300

    Force screen resolution to 1920x2160

---
+++ Assets/Scripts/DisplayInitializer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayInitializer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution(1920, 2160, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
