ee2ac5e478fc3cf8cf1877a0e08ca65cb7f34fb9
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-10-02 10:30:26 +0300

    Fix for falling asteroid behaviour. TODO refactor the whole animation if we swap to perspective projection

--- Assets/Scripts/Behaviours/FallingAsteroidBehaviour.cs
+++ Assets/Scripts/Behaviours/FallingAsteroidBehaviour.cs
@@ -5,13 +5,15 @@ public class FallingAsteroidBehaviour : AsteroidBehaviour {
 
 	private bool _animate = false;
 	private float _timeAcc = 0.0f;
-	private float _scaleFactorPerFrame = 1.025f;//TODO calc from animation length 
+	private float _scaleFactorPerFrame = 1.015f;//TODO calc from animation length 
 
 	private const float ANIMATION_LENGTH = 3.0f;
 
 	public override void destroyMe()
 	{
 		_animate = true;
+		//destroy the collider, since we're only animating at this point
+		Destroy(this.GetComponent<Collider>());
 	}
 
 	// Update is called once per frame
@@ -23,8 +25,6 @@ public class FallingAsteroidBehaviour : AsteroidBehaviour {
 			}
 			//since we have orthographic projection we need to animate scale
 			//with perspective projection we would just move it towards the camera
-			//TODO also increase Y, so that something can happen underneath the falling particle 
-			//(otherwise it's collider still gets the events)
 			this.transform.localScale = _scaleFactorPerFrame * this.transform.localScale;
 		}
 	}
